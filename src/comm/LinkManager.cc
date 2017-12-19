0de3397792ef5536c628dd9ce9460323e762f1c9
Jacob Walser <jwalser90@gmail.com>
2017-12-19 14:05:15 -0500

    Autoconnect to UDP port 14400
    This is to autoconnect to Water Linked system

--- src/comm/LinkManager.cc
+++ src/comm/LinkManager.cc
@@ -464,6 +464,13 @@ void LinkManager::_updateAutoConnectLinks(void)
         udpConfig->setDynamic(true);
         SharedLinkConfigurationPointer config = addConfiguration(udpConfig);
         createConnectedLink(config);
+
+        udpConfig = new UDPConfiguration("Water Linked GPS");
+        udpConfig->setLocalPort(14400);
+        udpConfig->setDynamic(true);
+        config = addConfiguration(udpConfig);
+        createConnectedLink(config);
+
         emit linkConfigurationsChanged();
     }
 
